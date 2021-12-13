4418ab7d506389d459238a7843e6b7b21d9d3a94
lispio <lispio@protonmail.com>
2021-12-13 11:30:12 +0100

    change url to dynamic url

--- mrSite/mrapp/urls.py
+++ mrSite/mrapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('recipes/<str:recipes_name>', views.recipes),
+    path('recipes/<str:recipes_name>', views.recipes, name="recipes"),
 ]
